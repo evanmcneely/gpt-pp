e5077bf56c471ef9cfdd5de9a84f63967327534c
Evan McNeely <evan.mcneely@leadpages.com>
2023-06-18 18:51:24 -0700

    UI class

---
+++ gpt_engineer/ui.py
@@ -0,0 +1,17 @@
+import typer
+
+
+class UI:
+    def error(message: str):
+        typer.secho(message, fg=typer.colors.RED)
+
+    def prompt(message: str) -> str:
+        return typer.prompt(message)
+
+    def success(message: str):
+        prefix = typer.style("✔ ", fg=typer.colors.GREEN, bold=True)
+        typer.echo(prefix + message)
+
+    def fail(message: str):
+        prefix = typer.style("✘ ", fg=typer.colors.RED, bold=True)
+        typer.echo(prefix + message)
