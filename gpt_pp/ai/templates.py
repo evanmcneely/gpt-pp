e05628f37981190988b957f2e645e44d90e60bb1
Evan McNeely <evan.mcneely@leadpages.com>
2023-07-07 15:09:40 -0700

    star of pr review

--- gpt_pp/ai/templates.py
+++ gpt_pp/ai/templates.py
@@ -60,3 +60,47 @@ Example output:
 
 {chat_history}
 """  # noqa
+
+
+make_PR_review_notes = """
+Create review comments for this pull request given the details below.
+
+{pr_details}
+
+PR diff:
+{pr_diff}
+
+--------
+
+Notes about reviewing a PR:
+Leave comments for specific positions in the diff when you have something constructive to say. Don't point out the obvious. Lean towards being mildly critical as you have high standards.
+
+The position value equals the number of lines down from the first "@@" hunk header in the file you want to add a comment. The line just below the "@@" line is position 1, the next line is position 2, and so on. The position in the diff continues to increase through lines of whitespace and additional hunks until the beginning of a new file. You cannot leave a comment on a position that is unchanged.
+
+Keep in mind that a good PR review is thoughtful and contructive with specific and actionable feedback on the changes. Include your overall thoughts on the pull request along with whether or not the PR should be approved, blocked or commented on at the end.
+    1. Approve (APRROVE) if everything looks good and you have only minor suggestions about the code.
+    2. Block (REQUEST_CHANGES) if there there are errors or bugs in the code.
+    3. Comment (COMMENT) if the code contains no bugs or errors but there are things that could be improved before approval.
+
+When you are done reviewing the pull request, respond with a summary of the PR details (including the owner, repo and pull request number) along with a list of the comments you wish to make. Finish with your overall thoughts
+
+Leav your comments the way a pirate would speak.
+
+Example - Format your response like this:
+
+Pull request details:
+<include the owner, repo and number of the PR>
+
+1. File path: path/to/file.py
+Position: 4
+Comment: "Anything you want to say... like a pirate"
+
+2. File path: path/to/anouther/file.py
+Position: 16
+Comment: "Anything you want to say ... like a pirate"
+
+... repeat for as many comments as you wish
+
+Overall: <leave overall thoughts like a pirate about the changes being proposed and the PR review including decision to approve, block or comment>
+
+Begin!"""  # noqa
